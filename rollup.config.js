d6b1c4e5f9e28a73707f167b2996a43bed99a02c
Sergey Melnikov <melnikov.s220@gmail.com>
2020-06-03 22:49:04 -0400

    es2015 output with esm modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,20 @@
 const typescript = require("rollup-plugin-typescript2");
 
 module.exports = {
-  input: "src/index.ts",
-  plugins: [typescript()],
-  output: {
-    file: "lib/lobx.js",
-    format: "umd",
-    name: "lobx",
-    sourcemap: true
-  }
+	input: "src/index.ts",
+	plugins: [typescript()],
+	output: [
+		{
+			file: "lib/lobx.js",
+			format: "umd",
+			name: "lobx",
+			sourcemap: true
+		},
+		{
+			file: "lib/lobx.module.js",
+			format: "es",
+			name: "lobx",
+			sourcemap: true
+		}
+	]
 };
